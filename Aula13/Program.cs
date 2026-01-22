9fb7fffe8f82689418ada5c69b4977080b24079f
Erika Lima <erikadeo.lima@hotmail.com>
2026-01-21 21:40:22 -0300

    feat: add initial project files and implement discount calculation logic with customer types

---
+++ Aula13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Program
+{
+    public static class Program
+    {
+        private const double fivePercentOfDiscount = 0.95;
+
+        private const double tenPercentOfDiscount = 0.90;
+
+        /* 
+        Troquei essas declaraçoes pelo Enum
+        private const int clientTypeOne = 1;
+        private const int clientTypeTwo = 2;
+        private const int clientTypeTree = 3; 
+        
+        */
+
+            public enum CustomerType
+        {
+            ClientTypeOne = 1,
+            ClientTypeTwo = 2,
+            ClientTypeThree = 3
+        }
+
+        public static double CalculeDiscount(double totalOrderValue, int custumerType)
+        {   
+            // C# não suporta object desestructuring?
+            // const { ClientTypeOne, ClientTypeTwo, ClientTypeTree } = CustomerType;
+            // não existe
+            if (custumerType == CustomerType.ClientTypeOne) return totalOrderValue * fivePercentOfDiscount;
+            if (custumerType == CustomerType.ClientTypeTwo) return totalOrderValue * tenPercentOfDiscount;
+            return totalOrderValue;
+        }
+
+        
+        public static void Main(string[] args)
+        {   
+            Console.Write("Discount calculation for Customer type 1 ");
+            Console.WriteLine(CalculeDiscount(100, clientTypeOne));
+
+            Console.Write("Discount calculation for Customer type 2 ");
+            Console.WriteLine(CalculeDiscount(100, clientTypeTwo));
+
+            Console.Write("Discount calculation for Customer type 3 ");
+            Console.WriteLine(CalculeDiscount(100, clientTypeTree));
+        }
+    }
+}
---
+++ exercicio_aula13/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+// Dupla: Erika Lima e Marília Lima
+
+public class Program
+{
+    private const double validValue = 0;
+
+    /* 
+
+    Camile Nós haviamos declarado o enum mas por algum motivo não compilou não entendemos o porque....
+
+
+    public enum CustomerType
+        {
+            ClientRegular = 1,
+            ClientPremium = 2,
+            ClientVip= 3
+        } 
+    */
+
+    private const int ClientRegular = 1;
+    private const int ClientPremium = 2;
+    private const int ClientVip= 3;
+
+    
+    private const double DiscountClientRegular = 0.05;
+    private const double DiscountClientPremium = 0.1;
+    private const double DiscountClientVip = 0.15;
+        
+
+    public static double CalculateFinalPayment(int quantity, double unitPriceItem, int clientType, bool hasDiscount){
+        double totalValue = 0;
+
+        if (quantity <= validValue || unitPriceItem <= validValue){ return 0; }
+
+
+        if (clientType == ClientRegular){
+            totalValue = quantity * unitPriceItem;
+            if (hasDiscount){
+                totalValue = totalValue - (totalValue * DiscountClientRegular);
+            }
+        }
+        else if (clientType == ClientPremium){
+            totalValue = quantity * unitPriceItem;
+            if (hasDiscount)
+            {
+                totalValue = totalValue - (totalValue * DiscountClientPremium);
+            }
+        }
+        else if (clientType == DiscountClientVip){
+            totalValue = quantity * unitPriceItem;
+            if (hasDiscount){ totalValue = totalValue - (totalValue * DiscountClientVip); }
+        }
+        else{
+            totalValue = quantity * unitPriceItem;
+        }
+
+
+        return totalValue;
+    }
+
+    public static void Main(string[] args)
+        {   
+            Console.Write("Discount calculation for Customer type Regular: ");
+            Console.WriteLine(CalculateFinalPayment(1,100,1,true));
+
+            
+        }
+}
